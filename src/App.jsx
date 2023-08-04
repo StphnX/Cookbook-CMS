4223c6e7211380617e5739e9ade49415cd3dc8c7
Oleksiy Prokopan <alexeyprokopan@gmail.com>
2023-08-04 16:59:22 +0200

    contentful connected

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { useState , useEffect} from 'react'
-import './App.css'
+import { useState, useEffect } from 'react';
+import Contentful from './Contentful';
+import './App.css';
 
 function App() {
-  
+  const [recipe, setRecipe] = useState([]);
+  const { getCookbook } = Contentful();
+  useEffect(() => {
+    getCookbook()
+      .then((res) => {
+        setRecipe(res);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
+  console.log(recipe[0].name);
   return (
     <>
-      
+      <h1>{recipe[0].name}</h1>
+      <img className="pic" src={recipe[0].img} />
+      <p>{recipe[0].description} /</p>
+      {recipe[0].ingredients.map((item) => {
+        return <p>{item}</p>;
+      })}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Contentful.jsx
+++ src/Contentful.jsx
@@ -1,35 +1,33 @@
-import { createClient } from "contentful";
+import { createClient } from 'contentful';
 
 const Contentful = () => {
-    const client = createClient({
-        space: "fvwgdnm4oux1",
-        accessToken: "0F1deS2w-aiyaJ1sP8kSQU3A0hdskYT2rTMMbqWNK5o",
-        host: "preview.contentful.com"
-    });
+  const client = createClient({
+    space: 'fvwgdnm4oux1',
+    accessToken: '0F1deS2w-aiyaJ1sP8kSQU3A0hdskYT2rTMMbqWNK5o',
+    host: 'preview.contentful.com',
+  });
 
-    const getCookbook = async () => {
-        try {
-            const entries = await client.getEntries({
-                content_type: "cookbook",
-                select: "fields"
-            });
-            console.log(entries)
-            const saniEntries = entries.items.map((item) => {
-                const name = item.fields.description;
-                const ingredients = item.fields.ingredients;
-                const description = item.fields.description;
-                const img = item.fields.img[0].fields.file.url;
+  const getCookbook = async () => {
+    try {
+      const entries = await client.getEntries({
+        content_type: 'cookbook',
+        select: 'fields',
+      });
+      const saniEntries = entries.items.map((item) => {
+        const name = item.fields.name;
+        const description = item.fields.description;
+        const ingredients = item.fields.ingredients;
+        const img = item.fields.img.fields.file.url;
+        return { name, description, ingredients, img };
+      });
 
-                return { name, description, ingredients, img };
-            });
+      return saniEntries;
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-            return saniEntries;
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    return { getCookbook };
+  return { getCookbook };
 };
 
 export default Contentful;
