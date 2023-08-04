2d29079ed1eb4b473f0e3db9a153cb37e421d25a
Oleksiy Prokopan <alexeyprokopan@gmail.com>
2023-08-04 17:14:39 +0200

    loading added

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,38 @@ import './App.css';
 function App() {
   const [recipe, setRecipe] = useState([]);
   const { getCookbook } = Contentful();
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     getCookbook()
       .then((res) => {
-        setRecipe(res);
+        if (res) {
+          setRecipe(res);
+        }
+        setLoading(false); // Set loading to false once the data is fetched
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        console.log(error);
+        setLoading(false); // Handle the error and set loading to false
+      });
   }, []);
 
-  console.log(recipe[0].name);
   return (
     <>
-      <h1>{recipe[0].name}</h1>
-      <img className="pic" src={recipe[0].img} />
-      <p>{recipe[0].description} /</p>
-      {recipe[0].ingredients.map((item, index) => {
-        return <p key={index}>{item}</p>;
-      })}
+      {loading ? (
+        <p>Loading...</p>
+      ) : recipe.length > 0 ? (
+        <>
+          <h1>{recipe[0].name}</h1>
+          <img className="pic" src={recipe[0].img} alt={recipe[0].name} />
+          <p>{recipe[0].description}</p>
+          {recipe[0].ingredients.map((item, index) => {
+            return <p key={index}>{item}</p>;
+          })}
+        </>
+      ) : (
+        <p>No recipe found.</p>
+      )}
     </>
   );
 }
